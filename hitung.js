a670c1d7b9e02db42bfe2862eb6ca8f0b7ce9e96
FaslulFaizi <faslulfaizi666@gmail.com>
2024-06-12 09:22:05 +0700

    Initial commit

---
+++ hitung.js
@@ -0,0 +1,6 @@
+const { tambah, kurang, kali, bagi } = require("./kalkulator");
+
+console.log(tambah(2, 2));
+console.log(kurang(2, 2));
+console.log(kali(3, 3));
+console.log(bagi(3, 3));
---
+++ kalkulator.js
@@ -0,0 +1,17 @@
+const tambah = (number1, number2) => {
+  return number1 + number2;
+};
+
+const kurang = (number1, number2) => {
+  return number1 - number2;
+};
+
+const kali = (number1, number2) => {
+  return number1 * number2;
+};
+
+const bagi = (number1, number2) => {
+  return number1 / number2;
+};
+
+module.exports = { tambah, kurang, bagi, kali };
